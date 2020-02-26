61e128eaf6e7a92c2daf41c061997c92b333c737
C272 <realrussianmanconfirmed@gmail.com>
2020-02-26 21:51:56 +0000

    Add nuspec (defaults) and logo.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DnD5e")]
+[assembly: AssemblyDescription("A Dungeons and Dragons 5th Edition API interface for C#.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("C272")]
+[assembly: AssemblyProduct("DnD5e.Properties")]
+[assembly: AssemblyCopyright("(c) C272, 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4a8ad173-4b79-4010-b0b6-e1eae6ef5d2d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
